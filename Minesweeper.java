4a3fdad86d143ccb4b82a22532c497d20c53055e
nihitvarmasagi <sagivarma24@gmail.com>
2019-11-06 18:32:01 -0600

    removed visited from minesweeper

--- Minesweeper.java
+++ Minesweeper.java
@@ -9,26 +9,19 @@ Yes, the solution passed all the test cases in leetcode.
 class Solution {
     public char[][] updateBoard(char[][] board, int[] click) {
 
-        //Direction array
         int[][] checkdir = {{-1,-1},{-1,0},{-1,1},{0,-1},{0,1},{1,-1},{1,0},{1,1}};
-
         int m = board.length; int n = board[0].length;
-        boolean[][] visited = new boolean[m][n];
         Queue<Integer> queue = new LinkedList<>();
         queue.add(click[0]); queue.add(click[1]);
 
         while(queue.size()>0){
-
-            //Check if current index value is M
             int x = queue.poll(); int y = queue.poll();
             if(board[x][y]=='M'){
                 board[x][y]='X';
                 break;
             }
-            else if(board[x][y]=='E'){ //If the index value is E
+            else if(board[x][y]=='E'){
                 int mines = 0;
-
-                //Count the mines
                 for(int[] each:checkdir){
                     int tempX = x+each[0]; int tempY = y+each[1];
                     if(tempX>=0 && tempX<m && tempY>=0 && tempY<n){
@@ -38,25 +31,22 @@ class Solution {
                     }
                 }
 
-                //If no mines are present add the unvisited indexes to the queue.
                 if(mines==0){
                     for(int[] each:checkdir){
                         int tempX = x+each[0]; int tempY = y+each[1];
-                        if(tempX>=0 && tempX<m && tempY>=0 && tempY<n && !visited[tempX][tempY]){
+                        if(tempX>=0 && tempX<m && tempY>=0 && tempY<n && board[tempX][tempY]=='E'){
                             queue.add(tempX); queue.add(tempY);
-                            visited[tempX][tempY] = true;
                         }
                     }
                     board[x][y]='B';
                 }
-                else{//If mines are present change the value of the index
+                else{
                     board[x][y] = (char)(mines+'0');
                 }
             }
 
-            visited[x][y]=true;
         }
 
         return board;
     }
-}
+}}
