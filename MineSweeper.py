94aab86c28494dd7cad39b60ba4a0e25c584153c
Shantanu609 <shantanu_shinde_@mail.fresnostate.edu>
2020-08-18 15:09:16 -0700

    Changed Time complexity for Minesweeper problem

--- MineSweeper.py
+++ MineSweeper.py
@@ -1,5 +1,5 @@
-# Time Complexity : O(n) 
-# Space Complexity :O(n)
+# Time Complexity : O(mxn) 
+# Space Complexity :O(mxn)
 # Did this code successfully run on Leetcode : Yes
 # Any problem you faced while coding this : No
 # Your code here along with comments explaining your approach
