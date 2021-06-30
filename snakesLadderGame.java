0e3ee2b376cc8378a18f77b93e991aacacbc4ef5
Kunj-97 <57332203+Kunj-97@users.noreply.github.com>
2021-06-29 23:24:55 -0400

    Update snakesLadderGame.java

--- snakesLadderGame.java
+++ snakesLadderGame.java
@@ -1,108 +1,66 @@
-// Time Complexity : O(m*n) total number of cells
-// Space Complexity : O(m*n)  total number of cells     
-// Did this code successfully run on Leetcode : Yes
-// Any problem you faced while coding this : after class solution
-
-
-// Your code here along with comments explaining your approach
-
-//The idea is to flatten the 2D board into 1D board. We do this to keep track of the cell number. The traversal is zigzag according to the problem definition. Also, we keep track of offset for positive numbers.
-//Then we do a simple BFS traversal to reach the end point, which is N-1 in this case.
-
+//Can use a BFS approach but easier to convert it fully to 1D array - complex to map the rows and colummns from start point to their actual i,j position 
+// convert the whole game to 1D array how the player sees it from [i=max][j=0] - easier 
+// use queue to load next six steps ( dice ) from  the current node -> if square not -1 , exhange value on square with the number and add to queue -> add to visited node to avoid repetetions while iterating nodes
+// BFS  approach TIME -> O(N^2)  || Space ->  O(N^2)
 
 class Solution {
     public int snakesAndLadders(int[][] board) {
- 
-        //base check
-        if(board == null || board.length == 0) return 0;
+        int N = 0;
+        if ( (N = board.length) == 0 ) return 0;
         
-        //find the total number of rows and cols
-        int row = board.length, col = board[0].length;
-        int N = row * col;
-        //initialize the array to flatten the 2D matrix, that will have all the elements
-        int[] flatBoard = new int[N];
+        int[] nBoard = new int[N*N + 1];
+        //int to use as index for the 1D array
+        int t = 1;
         
-        int i = row - 1;
-        int j = 0;
+        //boolean which check which way we have to traverse 
+        boolean left2right = true;
         
-        //order to traverse the col in each row
-        boolean leftToRight = true;
-        
-        //starting index of flatten array
-        int index = 0;
-        
-        while(i>=0 && j >=0){
-            
-            if(board[i][j] == -1){
-                flatBoard[index] = -1;
-            }else{
-                flatBoard[index] = board[i][j] - 1; //offset because array is from 0 to N-1, whereas board shows from 1 to N
+        // insert into array how the user would discover the numbers 1-> N
+        for(int i = N - 1; i >= 0; i--) {
+            if (left2right) {
+                for(int j = 0; j < N; j++) nBoard[t++] = board[i][j];
+            }else {
+                for(int j = N - 1; j >= 0; j--) nBoard[t++] = board[i][j];
             }
-            
-            index++;
-            
-            if(leftToRight){
-                j++;//j increments
-            }else{
-                j--;
-            }
-            
-            if(j>=col){
-                j--;
-                i--;
-                leftToRight = !leftToRight;
-            }
-            
-            if(j<0){
-                j++;
-                i--;
-                leftToRight = !leftToRight;
-            }
+            left2right = !left2right;
         }
+        // System.out.println(Arrays.toString(nBoard));
         
-        //we will use BFS to visit the nodes. So we will use queue to keep track of indices of cells
         Queue<Integer> q = new LinkedList<>();
-        q.add(0);
-        //mark it visited in-place by placing an arbitary value
-        flatBoard[0] = -2;
         
-        //need a value to keep track of minimum number of steps
-        int minSteps = 0;
+        //add 1 for first position 
+        q.add(1);
         
-        //iterate the queue
+        // counting steps along the way 
+        int steps = 0;
+        boolean[] visited = new boolean[N*N + 1];
+        visited[1] = true;
+        
+        
         while(!q.isEmpty()){
+            steps++;
+            int curCnt = q.size();
             
-            
-            //we need to keep track of node level which is equal to number of dices rolled
-            int size = q.size();
-            
-            for(i=0; i<size; i++){
-                int current = q.poll();
-                
-            //check if end is reached or not
-                System.out.println("current is: " + current);
-            if(current == N-1) return minSteps;
-            
-            //find it's neighbours. Here neighbours can be x+1 to x+6 whatever value the dice gives within 6.
-            for(int k=1; k<7; k++){
-                int neighbour = current + k;                
-                //check if neighbour is within bound and not visited
-                if(neighbour < N && flatBoard[neighbour] != -2){
-                    //if a positive value then either a ladder or snake is present
-                   if(flatBoard[neighbour] >=0){
-                        q.add(flatBoard[neighbour]);//directly move to that cell
-                    }else{
-                        q.add(neighbour);//else add the general cell
-                        } 
-                    //mark it visited
-                    flatBoard[neighbour] = -2;
-                    }                
-                } 
-            }        
-            //end of one dice, so one step completed
-            minSteps++;
+            // for all the nodes in the queue 
+            for(int i = 0; i < curCnt; i++) {
+                int square = q.poll();
+                // can add upto 6 to the node 
+                for(int j = 1; j <= 6; j++) {
+                    int next_square = square + j;
+                    //detect snake or ladder 
+                    //if the value on that square is not -1 ( go to value pointed on square )
+                    if (nBoard[next_square] != -1) next_square = nBoard[next_square];
+                    if (visited[next_square]) continue;
+                    // if end node reached 
+                    if (next_square == N*N) return steps;
+                    //add to visited nodes
+                    visited[next_square] = true;
+                    
+                    // add node to queue
+                    q.offer(next_square);
+                }
+            }
         }
-        
         return -1;
     }
 }
