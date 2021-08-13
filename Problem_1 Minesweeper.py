f3c4d41ba11473d28435ed26ea71bb5ff7f31cfb
sushmakallapur <sushma.kallapur@gmail.com>
2021-08-13 12:03:24 -0700

    Completed BFS-4

---
+++ Problem_1 Minesweeper.py
@@ -0,0 +1,42 @@
+# // Time Complexity : O(n^2)
+# // Space Complexity : O(n^2)
+# // Did this code successfully run on Leetcode : Yes
+# // Your code here along with comments explaining your approach
+
+class Solution:
+    def getMine(self, board, row, col, rows, cols):
+        mines = 0
+        for i, j in self.directions:
+            x, y = row + i, col + j
+            if x >= 0 and x < rows and y >= 0 and y < cols and board[x][y] == "M":
+                mines += 1
+        return mines
+
+    def updateBoard(self, board: List[List[str]], click: List[int]) -> List[List[str]]:
+        if not board:
+            return board
+
+        self.directions = [[0, 1], [1, 0], [-1, 0], [0, -1], [1, 1], [-1, -1], [1, -1], [-1, 1]]
+        rows, cols = len(board), len(board[0])
+
+        # if first click is on a mine itself
+        if board[click[0]][click[1]] == "M":
+            board[click[0]][click[1]] = "X"
+            return board
+
+        q = deque([click])
+        board[click[0]][click[1]] = "B"
+
+        while q:
+            r, c = q.popleft()
+            mineCount = self.getMine(board, r, c, rows, cols)  # get number of mines around the current coordinate
+            if mineCount == 0:  # if 0 mines are in the neighborhood of the current coordinate
+                for i, j in self.directions:
+                    x, y = r + i, c + j
+                    if x >= 0 and x < rows and y >= 0 and y < cols and board[x][y] == "E":  # if neighbor is empty
+                        q.append([x, y])
+                        board[x][y] = "B"
+            else:  # if there are more than 0 mines are in the neighborhood of the current coordinate
+                board[r][c] = str(mineCount)
+
+        return board
---
+++ Problem_2 SnakesAndLadders.py
@@ -0,0 +1,55 @@
+# // Time Complexity : O(mn)
+# // Space Complexity : O(mn)
+# // Did this code successfully run on Leetcode : Yes
+# // Your code here along with comments explaining your approach
+
+class Solution:
+    def snakesAndLadders(self, board: List[List[int]]) -> int:
+        n = len(board)
+        flatBoard = [0 for i in range(n*n)]
+        index = 0
+        i=n-1
+        j = 0
+        direction = 0
+
+        while i>=0 and j>=0:
+            if board[i][j] == -1:
+                flatBoard[index] = -1
+            else:
+                flatBoard[index] = board[i][j] - 1
+
+            index += 1
+
+            if direction % 2 == 0: #even numbered row
+                j += 1
+            else: #odd numbered row
+                j -= 1
+
+            if j == n: #reached past last column
+                i -= 1
+                direction += 1
+                j -= 1
+            if j < 0: #reached past first column
+                i -= 1
+                direction += 1
+                j += 1
+
+        q = deque([0])
+        level = 0
+        flatBoard[0] = -2
+        while q:
+            for _ in range(len(q)):
+                cur = q.popleft()
+                if cur == (len(flatBoard)-1): #if reached the end of board
+                    return level
+                #check all 6 sides of the die
+                for i in range(1, 7):
+                    new = cur + i
+                    if new <= (len(flatBoard)-1) and flatBoard[new] != -2:
+                        if flatBoard[new] == -1:
+                            q.append(new)
+                        else:
+                            q.append(flatBoard[new])
+                        flatBoard[new] = -2
+            level += 1
+        return -1
