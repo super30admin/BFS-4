615f92bb774462776481d6266211fea3624c3c25
Adhish Shrivastava <adhish70@gmail.com>
2022-01-07 12:26:38 -0500

    Initial Commit

---
+++ Problem-147 Minesweeper.py
@@ -0,0 +1,11 @@
+# 529. Minesweeper
+# https://leetcode.com/problems/minesweeper/
+
+# Logic: 
+
+# Time Complexity:
+# Space Complexity: 
+
+class Solution:
+    def updateBoard(self, board: List[List[str]], click: List[int]) -> List[List[str]]:
+        pass
---
+++ Problem-148 Snakes and Ladders.py
@@ -0,0 +1,11 @@
+# 909. Snakes and Ladders
+# https://leetcode.com/problems/snakes-and-ladders/
+
+# Logic: 
+
+# Time Complexity:
+# Space Complexity: 
+
+class Solution:
+    def snakesAndLadders(self, board) -> int:
+        pass
