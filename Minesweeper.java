5d315975fea0f8c350003ececc68b80c435f162c
skatkar <skatkar225@gmail.com>
2022-06-17 19:45:42 -0500

    BFS-4 submission

--- Minesweeper.java
+++ Minesweeper.java
@@ -52,6 +52,49 @@ public class Minesweeper {
         return board;
     }
 
+    public char[][] updateBoardDFS(char[][] board, int[] click) {
+        if(board == null || board.length == 0) return new char[][]{};
+
+        m = board.length;
+        n = board[0].length;
+
+        // If the click position has mine then mark it as X and return
+        if(board[click[0]][click[1]] == 'M') {
+            board[click[0]][click[1]] = 'X';
+            return board;
+        }
+        dirs = new int[][]{
+                {0,1},
+                {0,-1},
+                {1,0},
+                {-1,0},
+                {1,1},
+                {1,-1},
+                {-1,1},
+                {-1,-1}
+        };
+
+        dfs(board, click);
+        return board;
+    }
+
+    private void dfs(char[][] board, int[] click) {
+        if(click[0] < 0 || click[1] < 0 || click[0] == m || click[1] == n || board[click[0]][click[1]] != 'E')
+            return;
+
+        board[click[0]][click[1]] = 'B';
+        int count = countMines(board, click[0], click[1]);
+        if(count > 0){
+            board[click[0]][click[1]] = (char)(count + '0');
+        }else {
+            for(int[] dir : dirs) {
+                int r = click[0] + dir[0];
+                int c = click[1] + dir[1];
+                dfs(board, new int[]{r,c});
+            }
+        }
+    }
+
     // Check number of mines in the vicinity of the given click position
     private int countMines(char[][] board, int i, int j) {
         int count = 0;
