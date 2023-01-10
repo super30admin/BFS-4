e926d800b46d1d2d654a6efcea1babbbc4fd703e
Riddhi Jaju <riddhijaju2610@gmail.com>
2023-01-10 18:18:32 -0500

    Update Sample.java

--- Sample.java
+++ Sample.java
@@ -82,3 +82,76 @@ class Solution
     }
     
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+Problem-529 (Minesweeper)
+TC and SC-O(n*m)
+
+Solution-
+  
+class Solution {
+    int[][] dirs = {{-1,0},{0,-1},{-1,-1},{1,1},{-1,1},{1,-1},{0,1},{1,0}};
+    public char[][] updateBoard(char[][] board, int[] click)
+    {
+        if(board == null || board.length == 0) return board;
+        int m = board.length;
+        int n = board[0].length;
+        if(board[click[0]][click[1]] == 'M')
+        {
+            board[click[0]][click[1]] = 'X';
+            return board;
+        }
+        Queue<int[]> q = new LinkedList<>();
+        board[click[0]][click[1]] = 'B';
+        q.add(click);
+        while(!q.isEmpty())
+        {
+            int[] curr = q.poll();
+            int mines = neighborMines(curr,board, m,n);
+            if(mines>0)
+            {
+                board[curr[0]][curr[1]] = (char)(mines+'0');
+            }
+            else
+            {
+                for(int[] dir: dirs)
+                {
+                    int nr = curr[0]+dir[0];
+                    int nc = curr[1]+dir[1];
+                    if(nr>=0 && nr<m && nc>=0 && nc<n && board[nr][nc] == 'E'){
+                    board[nr][nc]='B';
+                    q.add(new int[]{nr,nc});
+                }
+            }
+        }
+            
+    }
+        return board;
+}
+    
+    private int neighborMines(int[] curr, char[][] board, int m, int n)
+    {
+        int count = 0;
+        for(int[] dir: dirs)
+        {
+                int nr = curr[0]+dir[0];
+                int nc = curr[1]+dir[1];
+                if(nr>=0 && nr<m && nc>=0 && nc<n && board[nr][nc] == 'M')
+                {
+                    count++;
+                }
+        }
+        return count;
+    }
+}
