84740ab2e08c6cd37bf980da0c8bac1fd1e4f541
Khushbu-Kareliya <154071841+Khushbu-Kareliya@users.noreply.github.com>
2024-01-24 19:57:27 -0600

    Update Leetcode_529_MineSweeper.java
    BFS approach added

--- Leetcode_529_MineSweeper.java
+++ Leetcode_529_MineSweeper.java
@@ -80,3 +80,97 @@ class Solution {
         return count;
     }
 }
+
+
+
+
+
+//BFS approach
+/*
+approach: 
+1. base case - check if the click itself is mine, mark it as X and return. 
+2.have a dirs array defined for all the neighbors 
+3.perform BFS. 
+4. add intial index of click to the queue. 
+5. BFS logic
+    a. when poll from the queue, mark it as visited. 
+    b. cunt mines = if greater than zero, dont explore babies, just assign count to current position.
+    c. if count is 0, that means explore the babies, and go to the dirs array, 
+    d. if babies have E, means not visited, than only add it to the queue. 
+6. when q becomes empty, we exit the while loop and return board     
+    
+    TC: O(m*n)
+    SC: O(m*n) - Queue space!
+*/
+
+
+class Solution {
+    int[][] dirs;
+    int m, n;
+    public char[][] updateBoard(char[][] board, int[] click) {
+        //base case
+        if(board == null || click == null) return board;
+        
+        //if click index == mine, print x and return board 
+        if(board[click[0]][click[1]] == 'M')
+        {
+            board[click[0]][click[1]] = 'X';
+            return board;
+        }
+        
+                              // Up  DOWN   Right  Left    left-upD  Rt-dn-d r-up   
+        this.dirs = new int[][] { { -1, 0 }, { 1, 0 }, { 0, 1 }, { 0, -1 }, { -1, -1 }, { 1, 1 }, { -1, 1 },
+                { 1, -1 } };
+        
+        this.m = board.length;
+        this.n = board[0].length;
+        
+       Queue<int[]> q = new LinkedList<>();
+          
+        q.add(click);
+        board[click[0]][click[1]] ='B'; //mark it visited
+        
+        while(!q.isEmpty())
+        {
+            int[] curr = q.poll();
+            
+          //if we check visited here, than we'll not optimize ,beucase when we add babies, we might be adding elemnts those are already in the Queue. 
+            
+            int count = countMines(board, curr);
+            if(count > 0) //we dont explore child
+            {
+                board[curr[0]][curr[1]] = (char)(count + '0'); //ASCII value of count convert it to character count.
+            }else
+            {
+                //explore the babies of curr
+                for(int[] dir : dirs)
+                {
+                    int r = dir[0] + curr[0];
+                    int c = dir[1] + curr[1];
+
+                    if(r >=0 && c >=0 && r<m && c < n && board[r][c] == 'E') //if not visited
+                    {
+                        board[r][c] = 'B';
+                        q.add(new int[]{r,c});
+                    }
+                }
+            }
+        }
+        return board;
+    }
+            
+    private int countMines(char[][] board, int[] click)
+    {
+        int count =0;
+        for(int[] dir: dirs)
+        {
+            int r = dir[0] + click[0];
+            int c = dir[1] + click[1];
+            
+            //check bounds and M
+            if(r >=0 && c >=0 && r<m && c < n && board[r][c] == 'M')
+                count++;
+        }
+        return count;
+    }
+}
